238e0294a0b5cb9b00794d5289f40788c9bf3335
Weiping <weiping@diancloud.com>
2016-12-24 18:30:11 +0800

    + SMS Test

---
+++ test/TestSms.php
@@ -0,0 +1,64 @@
+<?php 
+define('SEROOT', getenv('SEROOT') );
+define('TROOT', getenv('TROOT') );
+define('CWD', getenv('CWD') );
+define('APP_ROOT', getenv('APP_ROOT') );
+
+require_once( SEROOT . "/loader/Autoload.php" );
+
+
+use \Tuanduimao\Loader\App as App;
+use \Tuanduimao\Mem as Mem;
+use \Tuanduimao\Excp as Excp;
+use \Tuanduimao\Err as Err;
+use \Tuanduimao\Conf as Conf;
+use \Tuanduimao\Model as Model;
+use \Tuanduimao\Utils as Utils;
+
+/**
+ *注意手机号
+ * 信息测试
+ */
+class TestSms extends PHPUnit_Framework_TestCase {
+
+
+
+	function testSend() {
+
+		$Sms = App::M("sms",[
+				"AppID"=>"1400017564", 
+			 	"AppKey"=>"2b9f1e3ef8e81ebb5cf4f2b9d1433fe0"
+			]);
+
+		$resp = $Sms->send('13436431858',"尊敬的客户张三您好！您的白金会员卡已生效，请在3天内激活，感谢合作和支持。");
+
+		Utils::out( "testSend\n", $resp );
+
+		$this->assertEquals($resp['result'],0);
+
+	}
+
+
+	function testSendVoice() {
+
+		$Sms = App::M("sms",[
+				"AppID"=>"1400017564", 
+			 	"AppKey"=>"2b9f1e3ef8e81ebb5cf4f2b9d1433fe0"
+			]);
+
+		$resp = $Sms->sendvoice('13436431858',"尊敬的客户张三您好！您的白金会员卡已生效，请在3天内激活，感谢合作和支持。");
+
+		Utils::out( "testSendVoice\n", $resp );
+
+		$this->assertEquals($resp['result'],0);
+
+	}
+
+
+
+
+}
+
+
+
+ ?>
